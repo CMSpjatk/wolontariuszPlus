533324801c892ae98ec9d415b2600f893374c804
Adrian Mantur <s14237@pjwstk.edu.pl>
2018-11-28 22:57:43 +0100

    Added newest events to MainPage, added new CSS file mainpage-events and some changes in HomeController

--- WolontariuszPlus/Areas/Home/Controllers/HomeController.cs
+++ WolontariuszPlus/Areas/Home/Controllers/HomeController.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WolontariuszPlus.Areas.OrganizerPanelArea.Models;
 using WolontariuszPlus.Data;
+using WolontariuszPlus.Models;
 using WolontariuszPlus.ViewModels;
 
 namespace WolontariuszPlus.Areas.Home.Controllers
@@ -14,6 +17,9 @@ namespace WolontariuszPlus.Areas.Home.Controllers
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        public AppUser LoggedUser => _db.AppUsers.First(u => u.IdentityUserId
+                                                             == User.FindFirstValue(ClaimTypes.NameIdentifier));
+
         CMSDbContext _db;
 
         public HomeController(CMSDbContext db)
@@ -23,7 +29,22 @@ namespace WolontariuszPlus.Areas.Home.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var user = LoggedUser;
+
+
+            var vm = new EventsViewModel
+            {
+                EventViewModels =
+                    _db.Events
+                       .Where(e => e.Date >= DateTime.Now && e.Organizer == user)
+                       .OrderByDescending(e => e.Date)
+                       .Take(5)
+                       .ToList()
+                       .Select(e => CreateEventViewModelForDisplaying(e)),
+                ViewType = VolunteerPanelViewType.UPCOMING_EVENTS
+            };
+
+            return View("Index", vm);
         }
 
         public IActionResult About()
@@ -49,6 +70,22 @@ namespace WolontariuszPlus.Areas.Home.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private DisplayEventViewModel CreateEventViewModelForDisplaying(Event e)
+        {
+            var n = (e.Address.ApartmentNumber <= 0) ? " " : ("/" + e.Address.ApartmentNumber.ToString());
+
+            return new DisplayEventViewModel
+            {
+                EventId = e.EventId,
+                Name = e.Name,
+                Date = e.Date,
+                Address = $"ul. {e.Address.Street} {e.Address.BuildingNumber}{n}, {e.Address.PostalCode} {e.Address.City}",
+                Description = e.Description,
+                OrganizerName = $"{e.Organizer.FirstName} {e.Organizer.LastName}",
+                RequiredPoints = e.RequiredPoints
+            };
         }
     }
 }
