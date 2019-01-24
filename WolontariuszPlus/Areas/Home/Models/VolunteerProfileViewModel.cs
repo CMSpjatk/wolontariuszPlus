1f2c9204e998fcccd54ff0f822110214b2f073d1
Maciej Sadoś <s14051@pjwstk.edu.pl>
2019-01-24 03:32:40 +0100

    Aligned numbers in application

--- WolontariuszPlus/Areas/Home/Models/VolunteerProfileViewModel.cs
+++ WolontariuszPlus/Areas/Home/Models/VolunteerProfileViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,21 @@ namespace WolontariuszPlus.Areas.Home.Models
     public class VolunteerProfileViewModel
     {
         public int VolunteerId { get; set; }
+
+        [Display(Name = "Imię i nazwisko")]
         public string FullName { get; set; }
+
         public string Email { get; set; }
+
+        [Display(Name = "Numer telefonu")]
         public string PhoneNumber { get; set; }
+
+        [Display(Name = "Miasto")]
         public string City { get; set; }
+
         public ICollection<PastEventsViewModel> PastEventViewModelList { get; set; }
+
+        [Display(Name = "Punkty")]
         public int Points { get; set; }
     }
 }
--- WolontariuszPlus/Areas/Home/Models/VolunteerRankViewModel.cs
+++ WolontariuszPlus/Areas/Home/Models/VolunteerRankViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,11 @@ namespace WolontariuszPlus.Areas.Home.Models
     public class VolunteerRankViewModel
     {
         public int VolunteerId { get; set; }
+
+        [Display(Name = "Imię i nazwisko")]
         public string FullName { get; set; }
+
+        [Display(Name = "Punkty")]
         public int Points { get; set; }
     }
 }
